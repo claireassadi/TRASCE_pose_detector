621af14c39f7065a87afd5afc3c35eccfbb45e82
claireassadi <146730394+claireassadi@users.noreply.github.com>
2024-04-02 12:27:03 +0200

    Change in the readme file

--- examples/use_this.py
+++ examples/use_this.py
@@ -2,23 +2,23 @@ import posedetector
 
 """ Hi to use every part of this program you need to uncomment each function"""
 
-# 01 Add your video name here
+# Add your video name here
 file_name='sallinger'
 
-# 02 Change the format (mp4) if needed
+# Change the format (mp4) if needed
 video_file_name=f'{file_name}.mp4'
 
-# 03 run this to predict the keypoints
+""" 01 run this function to predict the keypoints"""
 key_stop_process="a"
 #posedetector.pose_detector_predict(video_file_name, key_stop_process)
 
-# 04 run this to rename the name of the actors in the json
+""" 04 run this function to rename the name of the actors in the json"""
 #posedetector.rename_actor(file_name)
 
-# 05 run this to add value if some are missing in the json file
+""" 05 run this function to add value if some are missing in the json file"""
 #posedetector.interpolate_missing_values(file_name)
 
-# 06 run this to see graph visualization of the json values
+""" 06 run this function to see graph visualization of the json values"""
 figure = "esther"
 point = "right_wrist"
 frame_interval=10
